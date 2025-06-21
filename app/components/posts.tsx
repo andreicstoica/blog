417f54e20bc6751b26bcb4f64198d57eb01f1f5a
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-06-21 16:10:11 -0400

    fixed alignment on home page

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -18,13 +18,15 @@ export function BlogPosts() {
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
+            className="flex flex-col space-y-1 mb-3"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 tabular-nums">
+            <div className="w-full flex md:flex-row flex-col md:space-y-0">
+              {/* Date Column */}
+              <p className="text-neutral-600 dark:text-neutral-400 tabular-nums md:w-32 flex-shrink-0">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
+              {/* Title Column */}
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
                 {post.metadata.title}
               </p>
