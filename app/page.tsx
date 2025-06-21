7decfe26fb66d152af8bcef311da1b72de327480
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-06-21 16:28:45 -0400

    used bun accidentally oops

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import { BlogPosts } from "app/components/posts";
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">My Blog</h1>
       <p className="mb-4">
         {`Hi, my name is Andrei. I'm interested in how technology can help people directly,
         especially as it relates to expression and the physical world. Having worked in tech
